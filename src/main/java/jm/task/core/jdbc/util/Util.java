de758b7bd3e3e69dd50cce2ed383484c8e9df747
Alex Shenderoff <alex.shenderoff@gmail.com>
2024-09-22 00:05:24 +0300

    first commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static jm.task.core.jdbc.common.Constant.*;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
